5217a48d2b293af8d0ed4bf631b3632d724b8884
ppetkov <p.petkov891@gmail.com>
2021-04-14 19:43:14 +0300

    add og meta

--- pages/_app.js
+++ pages/_app.js
@@ -4,10 +4,27 @@ import Head from "next/head";
 function MyApp({ Component, pageProps }) {
 	return (
 		<>
-      <Head>
-        <title>Game of Defi</title>
-        <meta name="description" content="Game of Defi" />
-      </Head>
+			<Head>
+				<title>Game of Defi</title>
+				<meta name="description" content="Game of Defi" />
+				<meta
+					property="og:url"
+					content="https://www.gameofdefi.finance"
+				/>
+				<meta property="og:type" content="article" />
+				<meta
+					property="og:title"
+					content="Game of Defi"
+				/>
+				<meta
+					property="og:description"
+					content="How much does culture influence creative thinking?"
+				/>
+				<meta
+					property="og:image"
+					content="https://www.gameofdefi.finance/images/logo.svg"
+				/>
+			</Head>
 			<Component {...pageProps} />
 		</>
 	);
